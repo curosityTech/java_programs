254caa7b2b74fef68cf0595359877212f90df91f
Mayank1681 <mayankchoudhary385@gmail.com>
2024-02-24 16:33:08 +0530

    adding adding SumOfFirstNNaturalNumber.java file

---
+++ src/questions/SumOfFirstNNaturalNumber.java
@@ -0,0 +1,18 @@
+package questions;
+import java.util.Scanner;
+public class SumOfFirstNNaturalNumber {
+    public static void main(String[] args) {
+
+        System.out.print("enter number: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        int total = sum(number);
+
+        System.out.println(total);
+    }
+
+    private static int sum(int i) {
+        return i*(i+1) / 2;
+    }
+}
