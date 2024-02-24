38a28e7ecaf528fbe4c63b350d88fa4c2ac32cb6
Mayank1681 <mayankchoudhary385@gmail.com>
2024-02-24 15:57:16 +0530

    adding EvenOddNumber.java file

---
+++ src/questions/EvenOddNumber.java
@@ -0,0 +1,23 @@
+package questions;
+import java.util.Scanner;
+
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter number: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("number is even");
+        }
+        else{
+            System.out.println("number is odd");
+        }
+
+        // Ternary method
+        /*
+        String status = number % 2 == 0 ? "number is even" : "number is odd";
+        System.out.println("status is: " + status);
+         */
+    }
+}
