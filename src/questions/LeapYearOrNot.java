126a03924268b5e625da6d19c20b9981b4c3570c
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-05 13:22:17 +0530

    adding LeapYearOrNot.java file.

---
+++ src/questions/LeapYearOrNot.java
@@ -0,0 +1,23 @@
+package questions;
+import java.util.Scanner;
+public class LeapYearOrNot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter year: ");
+
+        int year = scan.nextInt();
+
+        check(year);
+
+    }
+
+    private static void check(int year) {
+        if (year%400 == 0 || (year%4 == 0 && year%100 != 0)) {
+            System.out.println("It is a leap year");
+        }
+        else {
+            System.out.println("year is not a leap year");
+        }
+    }
+}
