08512c55b73ca69dba1260c66c261caca47eca30
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-05 16:59:03 +0530

    adding ReverseTheNumber.java file.

---
+++ src/questions/ReverseTheNumber.java
@@ -0,0 +1,24 @@
+package questions;
+import java.util.Scanner;
+
+public class ReverseTheNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int num = scan.nextInt();
+
+        int rev_num = reverse(num);
+        System.out.println("reverse number is: " + rev_num);
+    }
+
+    private static int reverse(int num) {
+        int rev=0, rem;
+        while(num != 0) {
+            rem = num % 10;
+            rev = (rev * 10) + rem;
+            num /= 10;
+        }
+        return rev;
+    }
+}
