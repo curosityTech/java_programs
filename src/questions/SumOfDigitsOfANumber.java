3efc3025de71d3d5217e6208bcdbe64d76d21a44
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-05 16:42:06 +0530

    adding SumOfDigitsOfANumber.java file.

---
+++ src/questions/SumOfDigitsOfANumber.java
@@ -0,0 +1,22 @@
+package questions;
+import java.util.Scanner;
+public class SumOfDigitsOfANumber {
+    public static void main(String[] args) {
+        System.out.print("enter number: ");
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        sum(num);
+    }
+
+    private static void sum(int num) {
+        int total = 0, digit;
+        while(num != 0) {
+            digit = num % 10;
+            total += digit;
+            num /= 10;
+        }
+        System.out.println("sum of digits are: " + total);
+    }
+}
