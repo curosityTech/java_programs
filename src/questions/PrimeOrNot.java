2db6580901cd91d3bc2f932810488a46f8622e50
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-05 13:51:56 +0530

    adding Project_Default.xml file.

---
+++ src/questions/PrimeOrNot.java
@@ -0,0 +1,35 @@
+package questions;
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int num = scan.nextInt();
+
+        check(num);
+    }
+
+    private static void check(int num) {
+        boolean isPrime = true;
+        if (num < 2) {
+            System.out.println("number is not prime");
+        }
+        else {
+            for (int i=2; i<sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("number is prime");
+        }
+        else {
+            System.out.println("number is not prime");
+        }
+    }
+}
