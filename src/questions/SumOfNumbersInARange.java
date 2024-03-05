01dcace48ee5d6716de959033e3a0fec42d6c3b4
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-05 12:28:14 +0530

    adding private method to seperate the functionality in SumOfNumbersInARange.java file.

--- src/questions/SumOfNumbersInARange.java
+++ src/questions/SumOfNumbersInARange.java
@@ -2,7 +2,6 @@ package questions;
 import java.util.Scanner;
 public class SumOfNumbersInARange {
     public static void main(String[] args) {
-        int sum = 0;
         System.out.print("Enter first number: ");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
@@ -10,10 +9,15 @@ public class SumOfNumbersInARange {
         System.out.print("Enter second number: ");
         int b = scan.nextInt();
 
+        int total = check(a, b);
+        System.out.println(total);
+    }
+
+    private static int check(int a, int b) {
+        int sum = 0;
         for(int i = a; i <= b; i++) {
             sum += i;
         }
-
-        System.out.println(sum);
+        return sum;
     }
 }
