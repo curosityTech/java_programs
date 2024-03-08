aedee7350ebdaaae0950fb7535221c4b10b43665
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-08 15:43:18 +0530

    adding FactorsOfNumber.java file.

---
+++ src/questions/FactorsOfNumber.java
@@ -0,0 +1,21 @@
+package questions;
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter the number whose factors you need to find: ");
+        int number = scan.nextInt();
+
+        factors(number);
+    }
+
+    private static void factors(int number) {
+        for(int i=1; i<=number; i++) {
+            if(number%i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
