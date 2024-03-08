00af045ce763b743aaaba504485ee214986d5155
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-08 15:31:49 +0530

    adding Factorial.java file.

---
+++ src/questions/PowerOfNumber.java
@@ -0,0 +1,25 @@
+package questions;
+import java.util.Scanner;
+
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter base number: ");
+        int base = scan.nextInt();
+
+        System.out.print("enter power: ");
+        int power = scan.nextInt();
+
+        int total = power(base, power);
+        System.out.println(total);
+    }
+
+    private static int power(int base, int power) {
+        int total=1;
+        for(int i=1; i<=power; i++) {
+            total *= base;
+        }
+        return total;
+    }
+}
