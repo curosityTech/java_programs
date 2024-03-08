53906e44375cc18d390b25c7ea313322de7ece08
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-08 12:53:31 +0530

    adding NumberIsPalindromeOrNot.java file.

---
+++ src/questions/NumberIsPalindromeOrNot.java
@@ -0,0 +1,30 @@
+package questions;
+import java.util.Scanner;
+
+public class NumberIsPalindromeOrNot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = scan.nextInt();
+
+        check(number);
+    }
+
+    private static void check(int number) {
+        int rem, rev=0, actual = number;
+        while(number != 0) {
+            rem = number%10;
+            rev = (rev*10) + rem;
+            number /= 10;
+        }
+
+        if(rev == actual) {
+            System.out.println("number is a palindrome");
+        }
+
+        else {
+            System.out.println("number is not palindrome");
+        }
+    }
+}
