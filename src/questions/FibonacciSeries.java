bb523e33a4af22964a73bada53d52c8237fdaa3f
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-08 15:08:12 +0530

    adding FibonacciSeries.java file.

---
+++ src/questions/FibonacciSeries.java
@@ -0,0 +1,29 @@
+package questions;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter nth value: ");
+        int number = scan.nextInt();
+
+        print(number);
+    }
+
+    private static void print(int number) {
+        int first_term = 0, second_term = 1;
+        int total;
+
+        System.out.println(first_term);
+        System.out.println(second_term);
+
+        for(int i=2; i<=number; i++) {
+            total = first_term + second_term;
+            first_term = second_term;
+            second_term = total;
+            System.out.println(total);
+        }
+
+    }
+}
