5bd69fae15266a60e455f3e7abbef7986e2fcf04
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-11 13:37:59 +0530

    adding PerfectNumber.java file.

---
+++ src/questions/PerfectNumber.java
@@ -0,0 +1,30 @@
+package questions;
+import java.util.Scanner;
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int number = scan.nextInt();
+
+        int check = sum(number);
+
+        if(check == number) {
+            System.out.println("It is a perfect number");
+        }
+        else {
+            System.out.println("It is not a perfect number");
+        }
+    }
+
+    public static int sum(int number) {
+        int total=0;
+        for(int i=1; i<number; i++) {
+            if(number%i == 0) {
+                total += i;
+            }
+        }
+        return total;
+    }
+
+}
