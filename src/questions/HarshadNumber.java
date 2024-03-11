01ed8fcd5f438134131b1246a3db53f62d0ac556
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-11 15:45:25 +0530

    adding HarshadNumber.java file.

---
+++ src/questions/HarshadNumber.java
@@ -0,0 +1,32 @@
+package questions;
+import java.util.Scanner;
+import static java.lang.Math.floor;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int number = scan.nextInt();
+
+        check(number);
+    }
+
+    private static void check(int number) {
+        double total = 0, actual=number;
+        int rem;
+        while(number>0) {
+            rem = number%10;
+            total += rem;
+            number /= 10;
+        }
+        double result = actual/total;
+
+        if(result == floor(result)) {
+            System.out.println("It is a harshad number");
+        }
+        else {
+            System.out.println("It is not a harshad number");
+        }
+    }
+}
