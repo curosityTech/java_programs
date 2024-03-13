98b33afa0b1b74472cbbd8f8525b31619d630699
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-13 15:40:02 +0530

    adding RemoveVowelsFromString.java file.

---
+++ src/questions/RemoveVowelsFromString.java
@@ -0,0 +1,30 @@
+package questions;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RemoveVowelsFromString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter string: ");
+        String s = scan.next();
+
+        scan.close();
+
+        StringBuilder toggleString = toggle(s);
+        System.out.println("toggle string: " + toggleString);
+    }
+
+    private static StringBuilder toggle(String s) {
+        StringBuilder s2 = new StringBuilder();
+        String regex = "[aeiouAEIOU]";
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(!Pattern.matches(regex, Character.toString(ch))) {
+                s2.append(ch);
+            }
+        }
+        return s2;
+    }
+}
