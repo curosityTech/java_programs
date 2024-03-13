8fa40dd9df97230330bbc0d93e952fb2f9853da5
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-13 11:58:39 +0530

    adding CharacterIsAlphabetOrNot.java file

---
+++ src/questions/CharacterIsAlphabetOrNot.java
@@ -0,0 +1,33 @@
+package questions;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/* To create a regular expression (regex) in Java, you typically use the Pattern class from the java.util.regex package.
+ You define a regex pattern as a string, and then compile it into a Pattern object. You can then use this Pattern object
+ to match against input strings using Matcher objects.
+ */
+
+
+public class CharacterIsAlphabetOrNot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter character: ");
+        char letter = scan.next().charAt(0);
+
+        scan.close();
+
+        // Define the regex pattern for alphabets
+        String regex = "[a-zA-Z]";
+
+        boolean isCharacterAlphabet = Pattern.matches(regex, Character.toString(letter));
+
+        if(isCharacterAlphabet) {
+            System.out.println("character is an alphabet");
+        }
+        else {
+            System.out.println("character is not an alphabet");
+        }
+    }
+
+}
