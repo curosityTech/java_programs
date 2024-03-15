7968584d19e6621c068961a7740194684d8d81a0
Mayank1681 <mayankchoudhary385@gmail.com>
2024-03-15 14:32:30 +0530

    added replace method with normal method to remove vowels in a string in RemoveVowelsFromString.java file.

--- src/questions/RemoveVowelsFromString.java
+++ src/questions/RemoveVowelsFromString.java
@@ -13,18 +13,26 @@ public class RemoveVowelsFromString {
 
         StringBuilder toggleString = toggle(s);
         System.out.println("toggle string: " + toggleString);
+
+        String replaceString = replace(s);
+        System.out.println("replace string: " + replaceString);
     }
+
 
     private static StringBuilder toggle(String s) {
         StringBuilder s2 = new StringBuilder();
         String regex = "[aeiouAEIOU]";
 
         for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
+            char ch  = s.charAt(i);
             if(!Pattern.matches(regex, Character.toString(ch))) {
                 s2.append(ch);
             }
         }
         return s2;
+    }
+
+    private static String replace(String s) {
+        return s.replaceAll("[aeiouAEIOU]", "");
     }
 }
